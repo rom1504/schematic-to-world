c4f97ea16a89444aff193c4b4a0a095923a0e37b
Romain Beaumont <romain.rom1@gmail.com>
2016-01-17 15:50:45 +0100

    add more tests and fix negative iniPos

--- test/test.js
+++ test/test.js
@@ -16,30 +16,37 @@ describe("add the schematic properly",function(){
 
   var world=new World(()=>new Chunk());
   var schem;
-  var iniPos=new Vec3(1000,65,78);
   it("read the schematic",()=> {
     return fs.readFile(__dirname+"/fantasy-fort.schematic")
       .then(data => schematicParseAsync(data))
       .then(sch => schem=sch)
   });
-  it("load the schematic",()=>  {
-    return fs.readFile(__dirname+"/fantasy-fort.schematic")
-      .then(data => addSchematicToWorld(data,world,iniPos))
-  });
 
-  it("had loaded the schematic properly",() => {
-    var ps=[];
-    for(let y=0;y<Math.min(10,schem.height);y++) {
-      // TODO: the full schema will be checked once prismarine-world has an good function to loop all the blocks in a cube
-      for(let z=0;z<Math.min(1000,schem.length);z++) {
-        for(let x=0;x<Math.min(10,schem.width);x++) {
-          ps.push(world.getBlockType(iniPos.offset(x,y,z))
-            .then(type=>{
-              assert.equal(type,schem.getBlock(x,y,z).type&0xff,"wrong type at "+x+","+y+","+z);
-            }))
+  var iniPoss=[new Vec3(1000,65,78),new Vec3(320,65,577),new Vec3(-320,65,-577)];
+
+  iniPoss.forEach(iniPos => {
+    describe("works with iniPos="+iniPos, () => {
+      it("load the schematic",()=>  {
+        return fs.readFile(__dirname+"/fantasy-fort.schematic")
+          .then(data => addSchematicToWorld(data,world,iniPos))
+      });
+
+      it("has loaded the schematic properly",() => {
+        var ps=[];
+        for(let y=0;y<Math.min(10,schem.height);y++) {
+          // TODO: the full schema will be checked once prismarine-world has an good function to loop all the blocks in a cube
+          for(let z=0;z<Math.min(1000,schem.length);z++) {
+            for(let x=0;x<Math.min(10,schem.width);x++) {
+              ps.push(world.getBlockType(iniPos.offset(x,y,z))
+                .then(type=>{
+                  assert.equal(type,schem.getBlock(x,y,z).type&0xff,"wrong type at "+x+","+y+","+z);
+                }))
+            }
+          }
         }
-      }
-    }
-    return Promise.all(ps);
+        return Promise.all(ps);
+      })
+    });
   })
+
 });
