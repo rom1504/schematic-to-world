47290614c7659d811d7592b77ef66f35aaa0a012
Romain Beaumont <romain.rom1@gmail.com>
2016-01-17 15:07:55 +0100

    fix by updating initialize of prismarine-chunk and prismarine-world

--- test/test.js
+++ test/test.js
@@ -16,9 +16,11 @@ describe("add the schematic properly",function(){
 
   var world=new World(()=>new Chunk());
   var schem;
-  var iniPos=new Vec3(1000,64,78);
+  var iniPos=new Vec3(1000,65,78);
   it("read the schematic",()=> {
-    return fs.readFile(__dirname+"/fantasy-fort.schematic").then(data => schematicParseAsync(data)).then(sch => schem=sch);
+    return fs.readFile(__dirname+"/fantasy-fort.schematic")
+      .then(data => schematicParseAsync(data))
+      .then(sch => schem=sch)
   });
   it("load the schematic",()=>  {
     return fs.readFile(__dirname+"/fantasy-fort.schematic")
@@ -27,10 +29,10 @@ describe("add the schematic properly",function(){
 
   it("had loaded the schematic properly",() => {
     var ps=[];
-    for(let y=20;y<Math.min(40,schem.height);y++) {
+    for(let y=0;y<Math.min(10,schem.height);y++) {
       // TODO: the full schema will be checked once prismarine-world has an good function to loop all the blocks in a cube
-      for(let z=0;z<Math.min(30,schem.length);z++) {
-        for(let x=0;x<Math.min(20,schem.width);x++) {
+      for(let z=0;z<Math.min(1000,schem.length);z++) {
+        for(let x=0;x<Math.min(10,schem.width);x++) {
           ps.push(world.getBlockType(iniPos.offset(x,y,z))
             .then(type=>{
               assert.equal(type,schem.getBlock(x,y,z).type&0xff,"wrong type at "+x+","+y+","+z);
